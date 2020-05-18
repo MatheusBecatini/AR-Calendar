768b781dd0a9aabe2f88deab3ff2e02336f672cd
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-05-18 19:41:56 -0300

    Config

---
+++ config.js
@@ -0,0 +1,20 @@
+const msalConfig = {
+    auth: {
+      clientId: '29173524-1b57-4e83-a90a-cdb3eddc22a8',
+      redirectUri: 'https://matheusbecatini.github.io/AR-Calendar/'
+    },
+    cache: {
+      cacheLocation: "sessionStorage",
+      storeAuthStateInCookie: false,
+      forceRefresh: true
+    }
+  };
+  
+  const loginRequest = {
+    scopes: [
+      'openid',
+      'profile',
+      'user.read',
+      'calendars.read'
+    ]
+  }
