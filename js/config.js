28ecb8a0ae52beeaae939ecb0fbfc993513455e6
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-05-22 15:47:37 -0300

    New Models

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const msalConfig = {
   auth: {
     clientId: '29173524-1b57-4e83-a90a-cdb3eddc22a8',
-    redirectUri: 'https://matheusbecatini.github.io/AR-Calendar/'
+    redirectUri: 'http://localhost:8080/'
   },
   cache: {
     cacheLocation: "sessionStorage",
